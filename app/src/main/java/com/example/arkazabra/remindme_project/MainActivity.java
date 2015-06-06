99af908c201ccab121bce5c010d81bcaadf9c578
Ekaterina <simigami89@bk.ru>
2015-06-06 17:21:12 +0300

    Create main activity and layout

---
+++ app/src/main/java/com/example/arkazabra/remindme_project/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.arkazabra.remindme_project;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by arkazabra on 06.06.15.
+ */
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle saveInstanceState){
+        super.onCreate(saveInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
